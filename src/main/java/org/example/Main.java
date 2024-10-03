bf1e8854c45a0f8724fa6eb30be62684c119ea15
KISB4LU <kbalazs4677@gmail.com>
2024-10-03 21:38:05 +0200

    már meg lehet jeleniteni a részvény árfolyamot

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,14 +10,14 @@ import javax.swing.*;
 public class Main{
 
     public static void main(String[] args) throws IOException {
-        HistoricalData hd = new HistoricalData();
-        String json = hd.GetData("AAPL","1Hour","100","sip");
+        /*HistoricalData hd = new HistoricalData();
+        String json = hd.GetData("AAPL","1Min","100","iex");
         System.out.println(json);
-        Chart[] AAPL = hd.GetChart("AAPL","1Hour","100","sip");
+        Chart[] AAPL = hd.GetChart("AAPL","1Min","100","iex");
         double min = AAPL[0].close();
         for (Chart c : AAPL) {
             System.out.println("Date: " + c.date() + " close: " + c.close());
-        }
+        }*/
         MyWindow window = new MyWindow();
         window.setVisible(true);
     }
--- src/main/java/window/Graph.java
+++ src/main/java/window/Graph.java
@@ -14,16 +14,16 @@ public class Graph extends JPanel {
     Chart(){
         image = new BufferedImage(200,100,BufferedImage.TYPE_INT_RGB);
     }*/
-    private int width = 400;
-    private int height = 200;
-    private BufferedImage draw(){
+    private int width = 600;
+    private int height = 400;
+    private BufferedImage LineGraph(){
         BufferedImage image = new BufferedImage(width, height,BufferedImage.TYPE_INT_RGB);
         Graphics2D g = image.createGraphics();
-
+        //g.drawLine(0,10,50,50);
         HistoricalData hd = new HistoricalData();
         Chart stock[] = null;
         try {
-            stock = hd.GetChart("AAPL","1Hour","100","iex");
+            stock = hd.GetChart("AAPL","5Min","1000","iex");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -38,16 +38,42 @@ public class Graph extends JPanel {
                 max = stock[i].close();
             }
         }
+        double Xdiff =width/stock.length;
         double x0 = 0;
-        double x1 = width/stock.length;
+        double x1 = Xdiff;
+        double Ydiff = height/(max-min);
         for(int i = 0; i<stock.length-1; i++){
-            double y0 = stock[i].close()/max;
-            double y1 = stock[i+1].close()/max;
-            y0 *= height;
-            y1 *= height;
+            double y0 = stock[i].close()-min;
+            double y1 = stock[i+1].close()-min;
+            y0 = height - y0*Ydiff;
+            y1 = height - y1*Ydiff;
             g.draw(new Line2D.Double(x0,y0,x1,y1));
-            x0 +=width/stock.length;
-            x1 +=width/stock.length;
+            x0 += Xdiff;
+            x1 += Xdiff;
+                System.out.println(i+". Date: " + stock[i].date() + " close: " + stock[i].close());
+        }
+        return image;
+    }
+    private BufferedImage Candlestick (){
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = image.createGraphics();
+        HistoricalData hd = new HistoricalData();
+
+        Chart stock[] = null;
+        try {
+            stock = hd.GetChart("AAPL","5Min","1000","iex");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        double min = stock[0].low();
+        double max = stock[0].high();
+        for(Chart c: stock){
+            if(c.low()<min){
+                min = c.low();
+            }
+            if(c.high()>max){
+                max = c.high();
+            }
         }
         return image;
     }
@@ -55,6 +81,6 @@ public class Graph extends JPanel {
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.drawImage(draw(), 0, 0, null);
+        g2d.drawImage(LineGraph(), 0, 0, null);
     }
 }
